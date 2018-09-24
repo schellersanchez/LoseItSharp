d5084a7f7bca1cebc9c849df0486ccd189f9d99a
schellersanchez <scheller.sanchez@gmail.com>
2018-09-23 21:21:13 -0700

    Initial code push

---
+++ LoseItSharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoseItSharp.Startup))]
+namespace LoseItSharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
