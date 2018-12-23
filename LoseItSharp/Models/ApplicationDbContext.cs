875d77b397d753aec9cd3701b241b2fc2f83d66a
schel <schel@DESKTOP-6JO7R5P>
2018-12-22 14:53:01 -1000

    Updated NuGet packages

---
+++ LoseItSharp/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoseItSharp.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //public DbSet<Step> Steps { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+}
