70028e5f64c9d810bc07d701c31f4ab6dbf06275
schel <schel@DESKTOP-6JO7R5P>
2018-12-23 16:30:58 -1000

    Create new matches and associated records (participant, match weeks, etc).

--- LoseItSharp/Controllers/MatchController.cs
+++ LoseItSharp/Controllers/MatchController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace LoseItSharp.Controllers
 {
@@ -59,16 +60,58 @@ namespace LoseItSharp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Add(Match Model)
         {
+            //Check if user is logged in
+            if (!User.Identity.IsAuthenticated)
+            {
+                ViewBag.Message = "Please log in to create";
+                return View(Model);
+            }
+
+            //Validate form
             if (!ModelState.IsValid)
             {
                 return View(Model);
             }
+            
+            //Get current user
+            string currentUserId = User.Identity.GetUserId();
+            ApplicationUser userInDb = _db.Users.Find(currentUserId);
 
-            Model.MatchEnd = Model.MatchStart.AddDays(Model.NumberOfWeeks * 7);
+            //Create new Match
+            Match newMatch = Model;
+            newMatch.CreatedById = currentUserId;
+            newMatch.MatchEnd = newMatch.MatchStart.AddDays(newMatch.NumberOfWeeks * 7).AddMinutes(-1); //11:59pm
             _db.Matches.Add(Model);
             _db.SaveChanges();
 
-            //TODO: create match weeks
+            //Add current user to match
+            Participant participant = new Participant()
+            {
+                Match = newMatch,
+                ApplicationUser = userInDb,
+                IsMatchAdmin = true,
+            };
+            _db.Participants.Add(participant);
+            _db.SaveChanges();
+
+            //Create MatchWeeks
+            DateTime weekStart = Model.MatchStart;
+            DateTime weekEnd = weekStart.AddDays(7).AddMinutes(-1); //11:59pm
+            for(int i = 0; i < Model.NumberOfWeeks; i++)
+            {
+                MatchWeek mw = new MatchWeek()
+                {
+                    Match = newMatch,
+                    StartDate = weekStart,
+                    EndDate = weekEnd,
+                    WeekNumber = i + 1
+                };
+                _db.MatchWeeks.Add(mw);
+                weekStart.AddDays(7);
+                weekEnd.AddDays(7);
+            }
+
+            _db.SaveChanges();
 
             return RedirectToAction("Index");
         }
